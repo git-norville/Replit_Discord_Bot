5c51afc965551b4158f558a05ea51fbe1ce488a0
Norville _ <89115468+git-norville@users.noreply.github.com>
2021-08-26 00:37:44 +0000

    put error handing for discord

--- main.py
+++ main.py
@@ -5,21 +5,21 @@ from discord.ext import commands
 import pi_maker
 from pi_maker import *
 my_secret = os.environ['DISCO_KEY']
-client = commands.Bot(command_prefix = "`")
+bot = commands.Bot(command_prefix = "`")
 
 keep_alive()
-@client.event
+@bot.event
 async def on_ready():
 	print("bot is ready")
 
-@client.command()
+@bot.command()
 async def ping(ctx):
-	await ctx.send(f"pong! {round(client.latency * 1000)}ms")
+	await ctx.send(f"pong! {round(bot.latency * 1000)}ms")
 
-@client.command()
-async def DYF(ctx, usr_imp):
+@bot.command()
+async def DYF(ctx, usr_imput):
 	ply_stats = list()
-	ply_stats = create_pi(usr_imp,ply_stats)
+	ply_stats = create_pi(usr_imput,ply_stats)
 	# tier = stats[0]["tier"]
 	# rank = stats[0]["rank"]
 	# lp = stats[0]["leaguePoints"]
@@ -27,4 +27,8 @@ async def DYF(ctx, usr_imp):
 
 	await ctx.send(file=discord.File('dyf.png'))
 
-client.run(my_secret)
+@bot.event
+async def on_command_error(ctx, error):
+    await ctx.send(f"An error occured: {str(error)}")
+
+bot.run(my_secret)
